4b2903b939623725946ae7515d4178ea5d00d0aa
Un pingüino <elantartico@protonmail.com>
2020-07-24 20:57:09 -0300

    Revertir "Cambio err dentro del query para probar."
    Esto revierte la confirmación 8a648c210a612f5b4521dea238d5444203d4db21.

--- server.js
+++ server.js
@@ -141,8 +141,7 @@ app.all("/" + process.env.BOT_ENDPOINT, function(request, response) {
   ];
   
 	client.query('SELECT id,value FROM botdelpueblo.data WHERE id = 1;', (err, res) => {
-		//if (err) throw err;
-		if (err) response.sendStatus(500);
+		if (err) throw err;
 		for (let row of res.rows) {
 			console.log(JSON.stringify(row));
 		}
